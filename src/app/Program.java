957890941f985cf5708b4bb46ae4f81f7f884879
Daniel Dias <danieldias.devjava@gmail.com>
2023-07-14 10:24:11 -0300

    Acrescentado Instant d04

--- src/app/Program.java
+++ src/app/Program.java
@@ -13,6 +13,7 @@ public class Program {
 		LocalDate d01 = LocalDate.parse("2022-07-20");
 		LocalDateTime d02 = LocalDateTime.parse("2022-07-20T02:30:34");
 		Instant d03 = Instant.parse("2022-07-20T02:30:34Z");
+		Instant d04 = Instant.parse("2022-05-22T04:00:13Z");
 		
 		LocalDate pastWeekLocalDate = d01.minusDays(7);
 		LocalDate nextWeekLocalDate = d01.plusDays(7);
@@ -22,6 +23,7 @@ public class Program {
 		
 		Instant pastWeekInstant = d03.minus(7, ChronoUnit.DAYS);
 		Instant nextWeekInstant = d03.plus(7, ChronoUnit.DAYS);
+		Instant pastWeeksInstant = d04.minus(6, ChronoUnit.DAYS);
 		
 		
 		System.out.println("pastWeekLocalDate = " + pastWeekLocalDate);
@@ -37,10 +39,12 @@ public class Program {
 		Duration t1 = Duration.between(pastWeekLocalDateTime, d02);
 		Duration t2 = Duration.between(pastWeekLocalDate.atStartOfDay(), d01.atStartOfDay());
 		Duration t3 = Duration.between(nextWeekInstant, d03);
+		Duration t4 = Duration.between(pastWeekInstant, pastWeeksInstant);
 		
 		System.out.println("t1 = " + t1.toDays());
 		System.out.println("t2 = " + t2.toDays());
 		System.out.println("t3 = " + t3.toDays());
+		System.out.println("t4 = " + t4.toHours());
 		
 	}
 
